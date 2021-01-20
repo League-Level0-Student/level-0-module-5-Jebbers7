ed9dec0c232ecc2774abc5bf5339b6a3782a7f1a
root <root@f72f9ec74f67>
2021-01-20 00:56:48 +0000

    Study Guide

--- src/_04_practice/_1_skill_practice/SkillPractice.java
+++ src/_04_practice/_1_skill_practice/SkillPractice.java
@@ -1,16 +1,20 @@
 package _04_practice._1_skill_practice;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class SkillPractice {
 public static void main(String[] args) {
 	
 	SkillPractice skills = new SkillPractice();
-    skills.skill1();
-    skills.skill2();
-    skills.skill3();
-    skills.skill4();
-    skills.skill5();
+	skills.skill1();
+	skills.skill2();
+	skills.skill3();
+	skills.skill4();
+	skills.skill5();
+  
+    
 }
 
 void skill1() {
@@ -23,9 +27,34 @@ void skill1() {
 		JOptionPane.showMessageDialog(null, "Eat your wheaties");
 	}}
 void skill2() {
-	// UNFINISHED \\
+	for (int i = 1; i < 31; i++) {
+		if(i % 3 == 0) {
+			System.out.println(i);
+		}
+	}}
+void skill3() {
+	Random rand = new Random();
+	int i = rand.nextInt(20);
+	System.out.println(i);
+	int j = rand.nextInt(10);
+	System.out.println(j);
+	JOptionPane.showMessageDialog(null,"The difference between the numbers is " + (i - j));
+}
+void skill4() {
+	String answer = JOptionPane.showInputDialog("What city do you live in?");
+	if(answer.equalsIgnoreCase("San Diego")) {
+		JOptionPane.showMessageDialog(null,"You live in America's finest city!");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "You should move to San Diego");
+	}
+	int cars = 3;
+	JOptionPane.showMessageDialog(null,"The cars have " + (cars*4) + " wheels between them.");
+}
+void skill5() {
+	String answer = JOptionPane.showInputDialog("What is the name of your school?");
+	JOptionPane.showMessageDialog(null, answer + "is a fantastic school!");
+}
 	
+}
 
-	
-}
-}
--- src/_04_practice/_2_code_4_life/Code4Life.java
+++ src/_04_practice/_2_code_4_life/Code4Life.java
@@ -14,22 +14,32 @@ public class Code4Life {
 	 * If you are not sure, ask your teacher 
 	 * *****************/
 	static boolean canPlaySounds = true;
+public static void main(String[] args) {
+	
 	
 	
 	// 1. Ask the user how many hours they spent coding this week.
-
+	String answer = JOptionPane.showInputDialog("How many hours did you spend coding this week?");
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+	int answerInt = Integer.parseInt(answer);
+	if(answerInt <= 2) {
+		JOptionPane.showMessageDialog(null, "Stop watching YouTube and write code instead!");
+	}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+	else if(answerInt >= 3 && answerInt <= 5) {
+		JOptionPane.showMessageDialog(null, "You are a Code Ninja!");
+	}
 	// 4. If it is more than 5, call the playBatmanTheme  method, then 
 	//    call the displayBatman method.
+	else {
+		playBatmanTheme();
+		displayBatman();
+	}
 
 
-
-	
+}
 	private static void playBatmanTheme() {
 	// NOTE: Download batman.wav from league-sounds on GitHub: https://github.com/jointheleague/league-sounds
 	//	     Then put batman.wav into the "_2_code_4_life" project
